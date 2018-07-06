5698a8ae53b9771f9ffe860571651cd9e87ecc3c
Digory Ellsworth <ellsworth.digory@gmail.com>
2018-07-06 15:20:32 -0600

    Commit Part2 Step1

--- src/component/Dashboard/Dashboard.js
+++ src/component/Dashboard/Dashboard.js
@@ -1,15 +1,39 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
 
 
 class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            myPosts: true,
+            posts: []
+        }
+    }
 
     render() {
+        let posts = this.state.posts.map((el) => {
+            return <Link to={`/posts/${el.posts_id}`} key={el.posts_id}>
+                <div>
+                <p>{el.title}</p>
+                    <div>
+                        <p>{el.author_id}</p>
+                        <img src={el.profile_pic} alt="User Picture"/>
+                    </div>
+                </div>
+            </Link>
+
+        })
         return(
             <div>
                 Dashboard
+                <button>Search</button>
+                <button>Reset</button>
             </div>
         )
     } 
 }
 
 export default Dashboard;
+
